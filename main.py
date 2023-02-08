643e45fb603227f72f5174857f5185c58daa98db
Nasrudin Bin Salim <Nasrudin.salim.suden@gmail.com>
2023-02-08 21:51:18 +0800

    Uses separate tokenizer and classifier

--- main.py
+++ main.py
@@ -1,23 +1,26 @@
 import os
-from transformers import pipeline
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, pipeline
 
-api_key = os.environ.get("KEY")
-classify = pipeline(model="papluca/xlm-roberta-base-language-detection")
+tokenizer = AutoTokenizer.from_pretrained("papluca/xlm-roberta-base-language-detection")
+model = AutoModelForSequenceClassification.from_pretrained("papluca/xlm-roberta-base-language-detection")
+classifier = pipeline("language-detector", model=model, tokenizer=tokenizer)
+api_key = os.getenv("CLIENT")
+
 
 
 def language_detection(request):
-  request_json = request.get_json()
+    request_json = request.get_json()
 
-  if request.args and 'key' in request.args:
-      key = request.args.get('key')
-  elif request_json and 'message' in request_json:
-      key = request_json['key']
-  
-  if key != api_key:
-    return "{message: 'Not Authorized', code:403}"
+    if request.args and 'key' in request.args:
+        key= request.args.get('key')
+    elif request_json and 'message' in request_json:
+      key= request_json['key']
 
-  if request.args and 'message' in request.args:
-      message= request.args.get('message')
-  elif request_json and 'message' in request_json:
-      message= request_json['message']
-  return classify(message)[0]
+    if key != api_key:
+        return "{message: 'Not Authorized', code:403}"
+
+    if request.args and 'message' in request.args:
+        message= request.args.get('message')
+    elif request_json and 'message' in request_json:
+        message= request_json['message']
+    return classifier(message)[0]
